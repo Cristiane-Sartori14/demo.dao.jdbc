0ab1da13f9c0985163dcd40c8118f7c25c1e7b34
Cristiane Sartori <sartori.cris@gmail.com>
2025-07-16 21:14:31 -0300

    Update DbException.java

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,13 +1,5 @@
 package db;
 
-<<<<<<< HEAD
-public class DbException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public DbException(String msg) {
-		super(msg);
-	}
-=======
 public class DbException extends RuntimeException{
 
 	
@@ -17,5 +9,4 @@ public class DbException extends RuntimeException{
 		super(msg);
 	}
 
->>>>>>> 8b836f5 (Inicializando o projeto)
 }
